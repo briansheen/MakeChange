7dd98bea66045f38580ae8c2f8b4895a30698cff
Brian Sheen <bsheen@gmail.com>
2017-09-22 17:41:04 -0500

    read line

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -18,33 +18,29 @@ public class Main {
         try (Scanner s = new Scanner(System.in)) {
             System.out.print("input amount of change to make (e.g. 4.50): ");
             while (s.hasNext()) {
-                String operator = s.next();
+                String operator = s.nextLine();
                 if (operator.equals("quit")) {
                     break;
                 }
-                try {
-                    Pattern pattern = Pattern.compile(MONEY_PATTERN);
-                    Matcher matcher = pattern.matcher(operator);
-                    if (matcher.matches()) {
-                        money = Double.valueOf(operator.replaceAll("[^\\d.]+", ""));
-                        dollars = (int) (money / 1);
-                        cents = Integer.valueOf(String.format("%.0f", ((money - dollars) * 100)));
-                        int numQuarters = (cents / QUARTER) + (dollars * 4);
-                        System.out.println(numQuarters + " quarters");
-                        int mod = cents % QUARTER;
-                        int numDimes = (mod / DIME);
-                        System.out.println(numDimes + " dimes");
-                        mod = mod % DIME;
-                        int numNickels = (mod / NICKEL);
-                        System.out.println(numNickels + " nickels");
-                        mod = mod % NICKEL;
-                        int numPennies = (mod / PENNY);
-                        System.out.println(numPennies + " pennies");
-                    } else {
-                        System.out.println("invalid input, amount must be positive, valid and follow pattern $x.yy");
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println("invalid input, amount must be a number and follow pattern $x.yy");
+                Pattern pattern = Pattern.compile(MONEY_PATTERN);
+                Matcher matcher = pattern.matcher(operator);
+                if (matcher.matches()) {
+                    money = Double.valueOf(operator.replaceAll("[^\\d.]+", ""));
+                    dollars = (int) (money / 1);
+                    cents = Integer.valueOf(String.format("%.0f", ((money - dollars) * 100)));
+                    int numQuarters = (cents / QUARTER) + (dollars * 4);
+                    System.out.println(numQuarters + " quarters");
+                    int mod = cents % QUARTER;
+                    int numDimes = (mod / DIME);
+                    System.out.println(numDimes + " dimes");
+                    mod = mod % DIME;
+                    int numNickels = (mod / NICKEL);
+                    System.out.println(numNickels + " nickels");
+                    mod = mod % NICKEL;
+                    int numPennies = (mod / PENNY);
+                    System.out.println(numPennies + " pennies");
+                } else {
+                    System.out.println("invalid input, amount must be a valid positive number and follow pattern $x.yy");
                 }
                 System.out.print("\ninput amount of change to make (e.g. 4.50): ");
             }
