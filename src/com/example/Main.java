ecdbe124bf09928ca23ad875effb7478197df717
Brian Sheen <bsheen@gmail.com>
2017-09-22 16:15:39 -0500

    added regex matcher

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import java.text.DecimalFormat;
-import java.text.ParseException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,7 +9,7 @@ public class Main {
     private static final int DIME = 10;
     private static final int NICKEL = 5;
     private static final int PENNY = 1;
-//    private static final String MONEY_PATTERN = "(([1-9]\\d*)?\\d))(\\.\\d\\d)?$";
+    private static final String MONEY_PATTERN = "^(\\$)?(\\d{1,3}(\\,\\d{3})*|(\\d+))(\\.\\d{2})?$";
 
     public static void main(String[] args) {
         Scanner s = null;
@@ -20,45 +18,38 @@ public class Main {
         int cents;
         try {
             s = new Scanner(System.in);
-            System.out.print("input amount of change to make (e.g. 4.50): $");
+            System.out.print("input amount of change to make (e.g. 4.50): ");
             while (s.hasNext()) {
                 String operator = s.next();
                 if (operator.equals("quit")) {
                     break;
                 }
                 try {
-//                    Pattern pattern = Pattern.compile(MONEY_PATTERN);
-//                    Matcher matcher = pattern.matcher(operator);
-//                    if(matcher.matches()){
-//
-//                    } else {
-//
-//                    }
-                    DecimalFormat df = new DecimalFormat("#.00");
-                    money = df.parse(operator).doubleValue();
-                    dollars = (int) (money / 1);
-                    String cent = String.format("%.0f",((money-dollars)*100));
-                    cents = Integer.valueOf(cent);
-                    if (money > 0) {
-                        int mod = 0;
-                        int numQuarters = (cents / QUARTER) + (dollars * 4);
-                        System.out.println(numQuarters + " quarters");
-                        mod = cents % QUARTER;
-                        int numDimes = (mod / DIME);
-                        System.out.println(numDimes + " dimes");
-                        mod = mod % DIME;
-                        int numNickels = (mod / NICKEL);
-                        System.out.println(numNickels + " nickels");
-                        mod = mod % NICKEL;
-                        int numPennies = (mod / PENNY);
-                        System.out.println(numPennies + " pennies");
+                    Pattern pattern = Pattern.compile(MONEY_PATTERN);
+                    Matcher matcher = pattern.matcher(operator);
+                    if(matcher.matches()){
+                        money = Double.valueOf(operator.replaceAll("[^\\d.]+", ""));
+                        dollars = (int) (money / 1);
+                        cents = Integer.valueOf(String.format("%.0f",((money-dollars)*100)));
+                            int mod;
+                            int numQuarters = (cents / QUARTER) + (dollars * 4);
+                            System.out.println(numQuarters + " quarters");
+                            mod = cents % QUARTER;
+                            int numDimes = (mod / DIME);
+                            System.out.println(numDimes + " dimes");
+                            mod = mod % DIME;
+                            int numNickels = (mod / NICKEL);
+                            System.out.println(numNickels + " nickels");
+                            mod = mod % NICKEL;
+                            int numPennies = (mod / PENNY);
+                            System.out.println(numPennies + " pennies");
                     } else {
-                        System.out.println("invalid input, must be positive and greater than zero");
+                        System.out.println("invalid input, amount must be positive, valid and follow pattern $x.yy");
                     }
-                } catch (NumberFormatException|ParseException e) {
-                    System.out.println("invalid input, double required");
+                } catch (NumberFormatException e) {
+                    System.out.println("invalid input, amount must be a number and follow pattern $x.yy");
                 }
-                System.out.print("input amount of change to make (e.g. 4.50): $");
+                System.out.print("\ninput amount of change to make (e.g. 4.50): ");
             }
         } finally {
             if (s != null) {
