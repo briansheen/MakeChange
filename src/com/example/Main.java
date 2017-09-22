9340576abe3e798c50066193705240b715259aa8
Brian Sheen <bsheen@gmail.com>
2017-09-22 17:16:22 -0500

    fixed formatting

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -27,22 +27,22 @@ public class Main {
                 try {
                     Pattern pattern = Pattern.compile(MONEY_PATTERN);
                     Matcher matcher = pattern.matcher(operator);
-                    if(matcher.matches()){
+                    if (matcher.matches()) {
                         money = Double.valueOf(operator.replaceAll("[^\\d.]+", ""));
                         dollars = (int) (money / 1);
-                        cents = Integer.valueOf(String.format("%.0f",((money-dollars)*100)));
-                            int mod;
-                            int numQuarters = (cents / QUARTER) + (dollars * 4);
-                            System.out.println(numQuarters + " quarters");
-                            mod = cents % QUARTER;
-                            int numDimes = (mod / DIME);
-                            System.out.println(numDimes + " dimes");
-                            mod = mod % DIME;
-                            int numNickels = (mod / NICKEL);
-                            System.out.println(numNickels + " nickels");
-                            mod = mod % NICKEL;
-                            int numPennies = (mod / PENNY);
-                            System.out.println(numPennies + " pennies");
+                        cents = Integer.valueOf(String.format("%.0f", ((money - dollars) * 100)));
+                        int mod;
+                        int numQuarters = (cents / QUARTER) + (dollars * 4);
+                        System.out.println(numQuarters + " quarters");
+                        mod = cents % QUARTER;
+                        int numDimes = (mod / DIME);
+                        System.out.println(numDimes + " dimes");
+                        mod = mod % DIME;
+                        int numNickels = (mod / NICKEL);
+                        System.out.println(numNickels + " nickels");
+                        mod = mod % NICKEL;
+                        int numPennies = (mod / PENNY);
+                        System.out.println(numPennies + " pennies");
                     } else {
                         System.out.println("invalid input, amount must be positive, valid and follow pattern $x.yy");
                     }
